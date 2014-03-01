6cffcfcb7a07dbf1964a15fb37454c9893d95c56
sarahlynn <sarah.holdijk@gmail.com>
2014-02-28 22:24:22 -0800

    setting up doxygen and gitignore

---
+++ Aegle/src/main.cpp
@@ -0,0 +1,18 @@
+/**
+@file main.cpp
+
+Created February 27th 2014
+*/
+
+#include <iostream>
+
+/**
+ * This is where the magic all starts
+ */
+int main()
+{
+	int in;
+
+  std::cout << "Hello World!";
+  std::cin >> in;
+}
